ee4db498749fe767c59cfd85ff58fce692d96b66
ThoulathIrfana <thoulathirfana@gmail.com>
2025-11-15 17:44:56 +0530

    Implement GroceryAgent with grocery list methods
    Added methods to generate and print a grocery list.

--- agents/grocery_agent.py
+++ agents/grocery_agent.py
@@ -1,1 +1,15 @@
+class GroceryAgent:
+    def generate_grocery_list(self, weekly_plan):
+        print("\n--- Generating Grocery List ---")
+        
+        base_items = [
+            "Rice", "Dal", "Vegetables", "Fruits",
+            "Milk", "Curd", "Wheat flour", "Oats"
+        ]
+
+        return base_items
+
+    def print_grocery_list(self, glist):
+        for item in glist:
+            print(f"- {item}")
 
